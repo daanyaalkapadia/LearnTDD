f18d8ed46a6bc79c441342604c7627a9b0f1f063
Daanyaal Kapadia <dkapadia@csod.com>
2024-10-05 16:15:32 +0530

    Module 1 : Starting from the basics

---
+++ Fibonacci.cs
@@ -0,0 +1,120 @@
+﻿using FluentAssertions;
+
+namespace LearnTDD
+{
+    public class FibonacciShould
+    {
+        [Fact]
+        public void Return_Expection_If_Position_Is_Negative()
+        {
+            //Arrange
+            int position = -4;
+
+            //Act
+            Action act = () => Fibonacci.GetElementAt(position);
+
+            //Assert
+            act.Should().Throw<ArgumentOutOfRangeException>()
+            .WithMessage("*Position should be positive*");
+        }
+        [Fact]
+        public void Return_0_If_Position_Is_0()
+        {
+            //Arrange
+            int position = 0;
+
+            //Act
+            var result = Fibonacci.GetElementAt(position);
+
+            //Assert
+            result.Should().Be(0);
+        }
+        [Fact]
+        public void Return_1_If_Position_Is_1()
+        {
+            //Arrange
+            int position = 1;
+
+            //Act
+            var result = Fibonacci.GetElementAt(position);
+
+            //Assert
+            result.Should().Be(1);
+        }
+        [Fact]
+        public void Return_1_If_Position_Is_2()
+        {
+            //Arrange
+            int position = 2;
+
+            //Act
+            var result = Fibonacci.GetElementAt(position);
+
+            //Assert
+            result.Should().Be(1);
+        }
+        [Fact]
+        public void Return_3_If_Position_Is_4()
+        {
+            //Arrange
+            int position = 4;
+
+            //Act
+            var result = Fibonacci.GetElementAt(position);
+
+            //Assert
+            result.Should().Be(3);
+        }
+        [Fact]
+        public void Return_13_If_Position_Is_7()
+        {
+            //Arrange
+            int position = 7;
+
+            //Act
+            var result = Fibonacci.GetElementAt(position);
+
+            //Assert
+            result.Should().Be(13);
+        }
+        [Fact]
+        public void Return_2584_If_Position_Is_18()
+        {
+            //Arrange
+            int position = 18;
+
+            //Act
+            var result = Fibonacci.GetElementAt(position);
+
+            //Assert
+            result.Should().Be(2584);
+        }
+        [Fact]
+        public void Return_102334155_If_Position_Is_40()
+        {
+            //Arrange
+            int position = 40;
+
+            //Act
+            var result = Fibonacci.GetElementAt(position);
+
+            //Assert
+            result.Should().Be(102334155);
+        }
+    }
+    public class Fibonacci
+    {
+        internal static int GetElementAt(int position)
+        {
+            if (position < 0)
+            {
+                throw new ArgumentOutOfRangeException("Position should be positive.");
+            }
+            if (position < 2)
+            {
+                return position;
+            }
+            return GetElementAt(position - 2) + GetElementAt(position - 1);
+        }
+    }
+}
---
+++ LeapYear.cs
@@ -0,0 +1,140 @@
+using FluentAssertions;
+
+namespace LearnTDD
+{
+
+    public class LeapYearShould
+    {
+        [Fact]
+        public void ReturnTrueFor4()
+        {
+            //Arrange
+            int number = 4;
+
+            //Act
+            bool result = LeapYear.Validate(number);
+
+            //Assert
+            result.Should().Be(true);
+        }
+        [Fact]
+        public void ReturnFalseFor5()
+        {
+            //Arrange
+            int number = 5;
+
+            //Act
+            bool result = LeapYear.Validate(number);
+
+            //Assert
+            result.Should().Be(false);
+        }
+        [Fact]
+        public void ReturnTrueFor16()
+        {
+            //Arrange
+            int number = 16;
+
+            //Act
+            bool result = LeapYear.Validate(number);
+
+            //Assert
+            result.Should().Be(true);
+        }
+        [Fact]
+        public void ReturnFalseFor100()
+        {
+            //Arrange
+            int number = 100;
+
+            //Act
+            bool result = LeapYear.Validate(number);
+
+            //Assert
+            result.Should().Be(false);
+        }
+        [Fact]
+        public void ReturnFalseFor200()
+        {
+            //Arrange
+            int number = 200;
+
+            //Act
+            bool result = LeapYear.Validate(number);
+
+            //Assert
+            result.Should().Be(false);
+        }
+        [Fact]
+        public void ReturnTrueFor400()
+        {
+            //Arrange
+            int number = 400;
+
+            //Act
+            bool result = LeapYear.Validate(number);
+
+            //Assert
+            result.Should().Be(true);
+        }
+        [Fact]
+        public void ReturnTrueFor800()
+        {
+            //Arrange
+            int number = 800;
+
+            //Act
+            bool result = LeapYear.Validate(number);
+
+            //Assert
+            result.Should().Be(true);
+        }
+        [Fact]
+        public void ReturnTrueFor2024()
+        {
+            //Arrange
+            int number = 2024;
+
+            //Act
+            bool result = LeapYear.Validate(number);
+
+            //Assert
+            result.Should().Be(true);
+        }
+        [Fact]
+        public void ReturnTrueFor2000()
+        {
+            //Arrange
+            int number = 2000;
+
+            //Act
+            bool result = LeapYear.Validate(number);
+
+            //Assert
+            result.Should().Be(true);
+        }
+        [Fact]
+        public void ReturnFalseFor2023()
+        {
+            //Arrange
+            int number = 2023;
+
+            //Act
+            bool result = LeapYear.Validate(number);
+
+            //Assert
+            result.Should().Be(false);
+        }
+    }
+    public class LeapYear
+    {
+        internal static bool Validate(int number)
+        {
+            if (number % 4 == 0 && (number % 100 != 0 || number % 400 == 0))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
