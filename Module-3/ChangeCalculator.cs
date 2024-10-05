df703b6b3d7ab877dbaa84d6b6ab937dea0cbd5e
Daanyaal Kapadia <dkapadia@csod.com>
2024-10-05 23:35:14 +0530

    Module 3 -  Pressured by the clock

---
+++ Module-3/ChangeCalculator.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using LearnTDD.Module_2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnTDD.Module_3
+{
+    public class ChangeCalculatorShould
+    {
+        private ChangeCalculator _changeCalculator;
+        public ChangeCalculatorShould()
+        {
+            _changeCalculator = new ChangeCalculator();
+        }
+        [Theory]
+        [InlineData(0.1,0.1)]
+        public void ReturnChange(float given, float toPay, params float[] change)
+        {
+            //Act
+            List<float> result = _changeCalculator.GetChange(given, toPay);
+
+            //Assert
+            for (int i = 0; i < result.Count; i++) 
+            {
+                result[i].Should().Be(change[i]);
+            }
+        }
+    }
+    public class ChangeCalculator
+    {
+        internal List<float> GetChange(float given, float toPay)
+        {
+            List<float> result = new List<float>();
+            return result;
+        }
+    }
+}
