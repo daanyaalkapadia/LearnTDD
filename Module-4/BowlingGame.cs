457980eee0115cf7171fcb07430349f0e0082082
Daanyaal Kapadia <dkapadia@csod.com>
2024-10-07 21:06:25 +0530

    Module 4 - Invalid Input

---
+++ Module-4/BowlingGame.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using LearnTDD.Module_2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnTDD.Module_4
+{
+    public class BowlingGameShould
+    {
+        private BowlingGame _bowlingGameShould;
+        public BowlingGameShould()
+        {
+            _bowlingGameShould = new BowlingGame();
+        }
+        [Theory]
+        [InlineData("")]
+        public void Return_0(string input)
+        {
+            Action act = () => _bowlingGameShould.Play(input);
+
+            act.Should().Throw<ArgumentException>()
+            .WithMessage("*Invalid Input*");
+        }
+    }
+    public class BowlingGame
+    {
+        internal int Play(string input)
+        {
+            throw new ArgumentException("Invalid Input");
+        }
+    }
+}
