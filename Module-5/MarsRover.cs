6f2976d6f6460a2e9a7dae4fa63b2f6bef9d18f8
Daanyaal Kapadia <dkapadia@csod.com>
2024-10-09 19:07:17 +0530

    Module 5 : Started with Invalid Input validation

---
+++ Module-5/MarsRover.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using LearnTDD.Module_4;
+
+namespace LearnTDD.Module_5
+{
+    public class MarsRoverShould
+    {
+        private MarsRover _marsRover;
+        public MarsRoverShould()
+        {
+            _marsRover = new MarsRover();
+        }
+        [Theory]
+        [MemberData(nameof(ChangeTestDataForInvalidInput))]
+        public void Throw_Error_For_Invalid_Input(string position, string command)
+        {
+            Action act = () => _marsRover.Drive(position, command);
+
+            act.Should().Throw<ArgumentException>()
+            .WithMessage("*Invalid Input*");
+        }
+        public static IEnumerable<object[]> ChangeTestDataForInvalidInput()
+        {
+            yield return new object[] { "A,0,0", "f" };
+        }
+    }
+    public class MarsRover
+    {
+        public string Drive(string position, string command)
+        {
+            if(position == "A,0,0")
+            {
+                throw new ArgumentException("Invalid Input");
+            }
+            return string.Empty;
+        }
+    }
+}
