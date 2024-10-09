1bf48f4fc49badf9ba0cd0702427b73420def5a4
Daanyaal Kapadia <dkapadia@csod.com>
2024-10-09 19:08:09 +0530

    Module 5 : Added Invalid test for direction it faces.

--- Module-5/MarsRover.cs
+++ Module-5/MarsRover.cs
@@ -22,6 +22,7 @@ namespace LearnTDD.Module_5
         public static IEnumerable<object[]> ChangeTestDataForInvalidInput()
         {
             yield return new object[] { "A,0,0", "f" };
+            yield return new object[] { "B,0,0", "f" };
         }
     }
     public class MarsRover
@@ -29,6 +30,10 @@ namespace LearnTDD.Module_5
         public string Drive(string position, string command)
         {
             if(position == "A,0,0")
+            {
+                throw new ArgumentException("Invalid Input");
+            }
+            if (position == "B,0,0")
             {
                 throw new ArgumentException("Invalid Input");
             }
