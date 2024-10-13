c2865d06980c9217866b8e67051d02bfc8341013
Daanyaal Kapadia <dkapadia@csod.com>
2024-10-13 17:43:12 +0530

    Module 7 : Updated test

--- Module-7/PokemonClassifier.cs
+++ Module-7/PokemonClassifier.cs
@@ -1,11 +1,5 @@
 ﻿using FluentAssertions;
-using LearnTDD.Module_6;
 using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LearnTDD.Module_7
 {
@@ -13,22 +7,24 @@ namespace LearnTDD.Module_7
     {
         private PokemonClassifier _pokemonClassifier;
         private Mock<IPokemonAPI> _pokemonAPI;
-        public PokemonClassifierShould() 
+        public PokemonClassifierShould()
         {
-            _pokemonClassifier = new PokemonClassifier();
             _pokemonAPI = new Mock<IPokemonAPI>();
+            _pokemonClassifier = new PokemonClassifier(_pokemonAPI.Object);
         }
         [Theory]
         [MemberData(nameof(ChangeTestData))]
-        public void Classify_Pokemon(int pokemonId,Pokemon pokemon, PokemonClassification expectedclassification)
+        public void Classify_Pokemon(int pokemonId, Pokemon pokemon, PokemonClassification expectedclassification)
         {
             _pokemonAPI.Setup(x => x.GetPokemonById(pokemonId)).Returns(pokemon);
             var actualclassification = _pokemonClassifier.Classify(pokemonId);
+
+            _pokemonAPI.Verify(x => x.GetPokemonById(pokemonId), Times.Once);
+            
             actualclassification.Should().Be(expectedclassification);
         }
         public static IEnumerable<object[]> ChangeTestData()
         {
-            //invalid direction
             yield return new object[] { 1, new Pokemon() { Name = "Charmander", Type = PokemonType.Fire }, PokemonClassification.GodTier };
             yield return new object[] { 2, new Pokemon() { Name = "Squirtle", Type = PokemonType.Water }, PokemonClassification.FilthyCasual };
         }
@@ -36,14 +32,14 @@ namespace LearnTDD.Module_7
     public class PokemonClassifier
     {
         private IPokemonAPI _pokemonAPI;
-        public PokemonClassifier() 
+        public PokemonClassifier(IPokemonAPI pokemonAPI)
         {
-            _pokemonAPI = new PokemonAPI();
+            _pokemonAPI = pokemonAPI;
         }
         public PokemonClassification Classify(int pokemonId)
         {
             var pokemon = _pokemonAPI.GetPokemonById(pokemonId);
-            if(pokemon.Type == PokemonType.Fire)
+            if (pokemon.Type == PokemonType.Fire)
             {
                 return PokemonClassification.GodTier;
             }
