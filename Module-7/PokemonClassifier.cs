925cb087eb389364b42d8b1c1dfc5547d6a3fa67
Daanyaal Kapadia <dkapadia@csod.com>
2024-10-13 17:22:34 +0530

    Module 7 : Pokemon Classifier Initial commit

---
+++ Module-7/PokemonClassifier.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnTDD.Module_7
+{
+    public class PokemonClassifierShould
+    {
+        private PokemonClassifier _pokemonClassifier;
+        public PokemonClassifierShould() 
+        {
+            _pokemonClassifier = new PokemonClassifier();
+        }
+        [Fact]
+        public void Classify_Pokemon()
+        {
+            var classification = _pokemonClassifier.Classify(1);
+            classification.Should().Be(PokemonClassification.GodTier);
+        }
+    }
+    public class PokemonClassifier
+    {
+        public PokemonClassification Classify(int pokemonId)
+        {
+            return PokemonClassification.GodTier;
+        }
+    }
+}
