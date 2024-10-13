742158f33d4c58339e10430f59fa2a882f34330a
Daanyaal Kapadia <dkapadia@csod.com>
2024-10-13 16:53:13 +0530

    Added private variable for pokemonApi

--- Module-7/PokemonAPI.cs
+++ Module-7/PokemonAPI.cs
@@ -4,13 +4,17 @@ namespace LearnTDD.Module_7
 {
     public class PokemonAPIShould
     {
+        private PokemonAPI _pokemonAPI;
+        public PokemonAPIShould()
+        {
+            _pokemonAPI = new PokemonAPI();
+        }
         [Theory]
         [InlineData(1, "Charmander", PokemonType.Fire)]
         [InlineData(2, "Squirtle", PokemonType.Water)]
         public void ReturnPokemonById(int id, string name, PokemonType pokemonType)
         {
-            PokemonAPI pokemonAPI = new PokemonAPI();
-            var pokemon = pokemonAPI.GetPokemonById(id);
+            var pokemon = _pokemonAPI.GetPokemonById(id);
             pokemon.Name.Should().Be(name);
             pokemon.Type.Should().Be(pokemonType);
         }
