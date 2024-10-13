e4a227094d3601ff9c85aaf96a5dbfe898a131fc
Daanyaal Kapadia <dkapadia@csod.com>
2024-10-13 17:26:16 +0530

    Module 7 : Added pokemon API call

--- Module-7/PokemonClassifier.cs
+++ Module-7/PokemonClassifier.cs
@@ -1,4 +1,6 @@
 ﻿using FluentAssertions;
+using LearnTDD.Module_6;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,9 +12,11 @@ namespace LearnTDD.Module_7
     public class PokemonClassifierShould
     {
         private PokemonClassifier _pokemonClassifier;
+        private Mock<IPokemonAPI> _pokemonAPI;
         public PokemonClassifierShould() 
         {
             _pokemonClassifier = new PokemonClassifier();
+            _pokemonAPI = new Mock<IPokemonAPI>();
         }
         [Fact]
         public void Classify_Pokemon()
@@ -23,9 +27,19 @@ namespace LearnTDD.Module_7
     }
     public class PokemonClassifier
     {
+        private IPokemonAPI _pokemonAPI;
+        public PokemonClassifier() 
+        {
+            _pokemonAPI = new PokemonAPI();
+        }
         public PokemonClassification Classify(int pokemonId)
         {
-            return PokemonClassification.GodTier;
+            var pokemon = _pokemonAPI.GetPokemonById(pokemonId);
+            if(pokemon.Type == PokemonType.Fire)
+            {
+                return PokemonClassification.GodTier;
+            }
+            return PokemonClassification.FilthyCasual;
         }
     }
 }
