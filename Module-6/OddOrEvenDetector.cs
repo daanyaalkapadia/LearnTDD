0517bf6df071a4177b2ef0c0d4dc13bf679e006e
Daanyaal Kapadia <dkapadia@csod.com>
2024-10-13 16:03:13 +0530

    Implemented OddOrEvenDetector class

---
+++ Module-6/OddOrEvenDetector.cs
@@ -0,0 +1,40 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnTDD.Module_6
+{
+    public class OddOrEvenDetectorShould
+    {
+        [Fact]
+        public void ReturnIsRandomNumberOdd()
+        {
+            var randomGeneratorMock = new Mock<IRandomGenerator>();
+            var oddDectector = new OddOrEvenDetector(randomGeneratorMock.Object);
+            oddDectector.IsRandomNumberOdd();
+            randomGeneratorMock.Verify(x => x.GetRandomBetween1And100(), Times.Once);
+        }
+    }
+    public class OddOrEvenDetector
+    {
+        private IRandomGenerator _randomGenerator;
+        public OddOrEvenDetector(IRandomGenerator randomGenerator)
+        {
+            _randomGenerator = randomGenerator;
+        }
+
+        public bool IsRandomNumberOdd()
+        {
+            var number = _randomGenerator.GetRandomBetween1And100();
+            if(number % 2 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+}
---
+++ Module-6/RandomGenerator.cs
@@ -0,0 +1,12 @@
+﻿namespace LearnTDD.Module_6
+{
+    public interface IRandomGenerator { int GetRandomBetween1And100(); }
+    public class RandomGenerator : IRandomGenerator
+    {
+        private static Random _random = new Random(); 
+        public int GetRandomBetween1And100()
+        {
+            return _random.Next(1, 101); // Returns a random number between 1 and 100 } }
+        }
+    }
+}
