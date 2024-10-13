6aff67d0a402c10b6759aa5e966537aa1e857f91
Daanyaal Kapadia <dkapadia@csod.com>
2024-10-13 16:09:48 +0530

    Module 6 : Added test for even

--- Module-6/OddOrEvenDetector.cs
+++ Module-6/OddOrEvenDetector.cs
@@ -26,9 +26,11 @@ namespace LearnTDD.Module_6
         [Theory]
         [InlineData(1, true)]
         [InlineData(3, true)]
+        [InlineData(4, false)]
+        [InlineData(8, false)]
         public void Return_IsRandomNumberOdd(int input, bool output)
         {
-            _randomGenerator.Setup(x => x.GetRandomBetween1And100()).Returns(0);
+            _randomGenerator.Setup(x => x.GetRandomBetween1And100()).Returns(input);
             var oddDectector = new OddOrEvenDetector(_randomGenerator.Object);
             var result = oddDectector.IsRandomNumberOdd();
             result.Should().Be(output);
@@ -47,9 +49,9 @@ namespace LearnTDD.Module_6
             var number = _randomGenerator.GetRandomBetween1And100();
             if(number % 2 == 0)
             {
-                return true;
+                return false;
             }
-            return false;
+            return true;
         }
     }
 
