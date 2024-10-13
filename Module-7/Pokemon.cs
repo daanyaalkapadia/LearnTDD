d73cbb589953c4d088e2309cb6cd048e2b7b8c36
Daanyaal Kapadia <dkapadia@csod.com>
2024-10-13 16:42:40 +0530

    Module 7 : Adding test to Pokemon API

---
+++ Module-7/Pokemon.cs
@@ -0,0 +1,12 @@
+﻿namespace LearnTDD.Module_7
+{
+    public enum PokemonType { Fire, Water, Grass, Rock }
+
+    public enum PokemonClassification { FilthyCasual = 1, GodTier = 2 }
+
+    public class Pokemon
+    {
+        public string Name { get; set; }
+        public PokemonType Type { get; set; }
+    }
+}
---
+++ Module-7/PokemonAPI.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnTDD.Module_7
+{
+    public class PokemonAPIShould
+    {
+        [Fact]
+        public void ReturnPokemonById()
+        {
+            PokemonAPI pokemonAPI = new PokemonAPI();
+            var pokemon = pokemonAPI.GetPokemonById(1);
+            pokemon.Should().NotBeNull();
+        }
+    }
+    public class PokemonAPI
+    {
+        public Pokemon GetPokemonById(int id)
+        {
+            return new Pokemon();
+        }
+    }
+}
