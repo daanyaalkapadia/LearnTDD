307a5ef5fb523d4853bdf4834a890f24b88880ce
Daanyaal Kapadia <dkapadia@csod.com>
2024-10-13 16:49:05 +0530

    Adding logic to pokemon API

--- Module-7/PokemonAPI.cs
+++ Module-7/PokemonAPI.cs
@@ -9,19 +9,30 @@ namespace LearnTDD.Module_7
 {
     public class PokemonAPIShould
     {
-        [Fact]
-        public void ReturnPokemonById()
+        [Theory]
+        [InlineData(1, "Charmander", PokemonType.Fire)]
+        [InlineData(2, "Squirtle", PokemonType.Water)]
+        public void ReturnPokemonById(int id, string name, PokemonType pokemonType)
         {
             PokemonAPI pokemonAPI = new PokemonAPI();
-            var pokemon = pokemonAPI.GetPokemonById(1);
-            pokemon.Should().NotBeNull();
+            var pokemon = pokemonAPI.GetPokemonById(id);
+            pokemon.Name.Should().Be(name);
+            pokemon.Type.Should().Be(pokemonType);
         }
     }
     public class PokemonAPI
     {
         public Pokemon GetPokemonById(int id)
         {
-            return new Pokemon();
+            switch (id)
+            {
+                case 1:
+                    return new Pokemon { Name = "Charmander", Type = PokemonType.Fire };
+                case 2:
+                    return new Pokemon { Name = "Squirtle", Type = PokemonType.Water };
+                default:
+                    return null;
+            }
         }
     }
 }
