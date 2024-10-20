8f17c6ee1015cc3303a9c559335c457671de138d
Daanyaal Kapadia <dkapadia@csod.com>
2024-10-20 14:34:12 +0530

    Module 8 : A little refactoring

--- Module-8/DigimonController.cs
+++ Module-8/DigimonController.cs
@@ -5,61 +5,70 @@ namespace LearnTDD.Module_8
 {
     public class DigimonControllerShould
     {
-        private readonly DigimonController _digitalController;
+        private readonly DigimonController _digimonController;
         private readonly Mock<IDigimonAPI> _digimonAPI;
         private readonly Mock<IDigimonAuthenticationAPI> _digimonAuthenticationAPI;
+
         public DigimonControllerShould()
         {
             _digimonAPI = new Mock<IDigimonAPI>();
             _digimonAuthenticationAPI = new Mock<IDigimonAuthenticationAPI>();
-            _digitalController = new DigimonController(_digimonAPI.Object, _digimonAuthenticationAPI.Object);
+            _digimonController = new DigimonController(_digimonAPI.Object, _digimonAuthenticationAPI.Object);
         }
 
         [Theory]
-        [InlineData("dk", "dk", 1, "arkaldjfasdion", "Daanyaal")]
-        public async Task Return_Name(string login, string password, int id, string token, string expectedName)
+        [InlineData("dk", "dk", 1, "valid-token", "Daanyaal")]
+        public async Task Return_Name_When_Valid_Credentials(string login, string password, int id, string token, string expectedName)
         {
             _digimonAuthenticationAPI.Setup(x => x.GetToken(login, password)).ReturnsAsync(token);
             _digimonAPI.Setup(x => x.GetNameById(token, id)).ReturnsAsync(expectedName);
-            var result = await _digitalController.GetDigimon(login, password, id);
+
+            var result = await _digimonController.GetDigimon(login, password, id);
 
             result.Should().Be(expectedName);
         }
+
         [Theory]
-        [InlineData("dk", "sdfsddfs", 1)]
-        public async Task Have_UnauthorizedAccess(string login, string password, int id)
+        [InlineData("dk", "invalid-password", 1)]
+        public async Task Throw_UnauthorizedAccessException_When_Invalid_Credentials(string login, string password, int id)
         {
             _digimonAuthenticationAPI.Setup(x => x.GetToken(login, password)).ThrowsAsync(new UnauthorizedAccessException());
 
-            Func<Task<string>> act = async () => await _digitalController.GetDigimon(login, password, id);
+            Func<Task> act = async () => await _digimonController.GetDigimon(login, password, id);
 
             await act.Should().ThrowAsync<UnauthorizedAccessException>();
         }
+
         [Theory]
-        [InlineData("dk", "dk", 1, "arkaldjfasdion")]
-        public async Task Have_API_Exception(string login, string password, int id, string token)
+        [InlineData("dk", "dk", 1, "valid-token")]
+        public async Task Throw_ApiException_When_Api_Fails(string login, string password, int id, string token)
         {
             _digimonAuthenticationAPI.Setup(x => x.GetToken(login, password)).ReturnsAsync(token);
-            _digimonAPI.Setup(x => x.GetNameById(token, id)).ThrowsAsync(new Exception());
-            Func<Task<string>> act = async () => await _digitalController.GetDigimon(login, password, id);
+            _digimonAPI.Setup(x => x.GetNameById(token, id)).ThrowsAsync(new Exception("API error"));
 
-            await act.Should().ThrowAsync<Exception>();
+            Func<Task> act = async () => await _digimonController.GetDigimon(login, password, id);
+
+            await act.Should().ThrowAsync<Exception>().WithMessage("API Failure : API error");
         }
+
         [Theory]
-        [InlineData("dk", "dk", 45, "arkaldjfasdion")]
-        public async Task Have_Key_Not_Found_Exception(string login, string password, int id, string token)
+        [InlineData("dk", "dk", 45, "valid-token")]
+        public async Task Throw_KeyNotFoundException_When_Digimon_Not_Found(string login, string password, int id, string token)
         {
             _digimonAuthenticationAPI.Setup(x => x.GetToken(login, password)).ReturnsAsync(token);
-            _digimonAPI.Setup(x => x.GetNameById(token, id)).ThrowsAsync(new KeyNotFoundException());
-            Func<Task<string>> act = async () => await _digitalController.GetDigimon(login, password, id);
+            _digimonAPI.Setup(x => x.GetNameById(token, id)).ThrowsAsync(new KeyNotFoundException("Digimon not found"));
 
-            await act.Should().ThrowAsync<KeyNotFoundException>();
+            Func<Task> act = async () => await _digimonController.GetDigimon(login, password, id);
+
+            await act.Should().ThrowAsync<KeyNotFoundException>().WithMessage("Mission Key : Digimon not found");
         }
     }
+
     public class DigimonController
     {
         private readonly IDigimonAPI _digimonAPI;
         private readonly IDigimonAuthenticationAPI _digimonAuthenticationAPI;
+
         public DigimonController(IDigimonAPI digimonAPI, IDigimonAuthenticationAPI digimonAuthenticationAPI)
         {
             _digimonAPI = digimonAPI;
@@ -73,11 +82,11 @@ namespace LearnTDD.Module_8
                 var token = await _digimonAuthenticationAPI.GetToken(login, password);
                 return await _digimonAPI.GetNameById(token, id);
             }
-            catch(KeyNotFoundException ex)
+            catch (KeyNotFoundException ex)
             {
                 throw new KeyNotFoundException("Mission Key : " + ex.Message);
             }
-            catch(UnauthorizedAccessException ex)
+            catch (UnauthorizedAccessException ex)
             {
                 throw new UnauthorizedAccessException("Unauthorized : " + ex.Message);
             }
