ffc96c0b1592cdbb41fa8dbb6bdcc09a764c61ac
Daanyaal Kapadia <dkapadia@csod.com>
2024-10-20 12:19:25 +0530

    Module 8 : Added Same test

--- Module-8/DigimonController.cs
+++ Module-8/DigimonController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FluentAssertions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,28 @@ namespace LearnTDD.Module_8
 {
     public class DigimonControllerShould
     {
+        private readonly DigimonController _digitalController;
+        public DigimonControllerShould()
+        {
+            _digitalController = new DigimonController();
+        }
 
+        [Fact]
+        public void Return_Name()
+        {
+            string login = "dk";
+            string password = "dk";
+            int id = 1;
+            var result = _digitalController.GetNameById(login, password, id);
+
+            result.Should().Be("");
+        }
     }
     public class DigimonController
     {
+        public string GetNameById(string login, string password, int id)
+        {
+            return "";
+        }
     }
 }
